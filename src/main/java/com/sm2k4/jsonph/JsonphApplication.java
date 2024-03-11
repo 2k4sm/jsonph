31dc2601447745be79d16ddeabe8636a08c6b601
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-03-11 21:55:39 +0530

    initial commit.

---
+++ src/main/java/com/sm2k4/jsonph/JsonphApplication.java
@@ -0,0 +1,13 @@
+package com.sm2k4.jsonph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm2k4/jsonph/JsonphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm2k4.jsonph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
